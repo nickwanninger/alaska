801eca55a6a7464471b81d2b029847fcfdbed5e1
Nick Wanninger <nickwanninger@gmail.com>
2022-11-16 20:08:18 -0600

    compile benchmarks with tools/alaska

--- tools/build_bench.sh
+++ tools/build_bench.sh
@@ -15,23 +15,23 @@ do
 	make -C test/npb $bench CLASS=$CLASS
 	mv $DST/$bench.$CLASS $DST/nas.$bench.base
 	get-bc $DST/nas.$bench.base >/dev/null
-	${ALASKA}/tools/acc $DST/nas.$bench.base.bc -lm -o $DST/nas.$bench.transformed >/dev/null
+	${ALASKA}/tools/alaska $DST/nas.$bench.base.bc -lm -o $DST/nas.$bench.transformed >/dev/null
 	# rm -f $DST/nas.$bench.base.bc
 done
 
-# compile gap
-for bench in bfs bc cc cc_sv pr pr_spmv sssp # tc
-do
-	echo "  CC  " $bench
-	gclang++ -std=c++11 -O3 -Wall ${ALASKA}/test/gapbs/src/${bench}.cc -o $DST/gap.$bench.base
-	echo "  TX  " $bench
-	get-bc $DST/gap.$bench.base >/dev/null
-	${ALASKA}/tools/acc $DST/gap.$bench.base.bc -o $DST/gap.$bench.transformed
-	# rm -f $DST/gap.$bench.base.bc
-done
+# # compile gap
+# for bench in bfs bc cc cc_sv pr pr_spmv sssp # tc
+# do
+# 	echo "  CC  " $bench
+# 	gclang++ -std=c++11 -O3 -Wall ${ALASKA}/test/gapbs/src/${bench}.cc -o $DST/gap.$bench.base
+# 	echo "  TX  " $bench
+# 	get-bc $DST/gap.$bench.base >/dev/null
+# 	${ALASKA}/tools/alaska $DST/gap.$bench.base.bc -o $DST/gap.$bench.transformed
+# 	# rm -f $DST/gap.$bench.base.bc
+# done
 
 
 echo "Compiling SQLite for fun... Wait a bit"
 gclang -Wall ${ALASKA}/test/sqlite/*.c -ldl -lm -pthread -o $DST/sqlite.base
 get-bc $DST/sqlite.base >/dev/null
-${ALASKA}/tools/acc $DST/sqlite.base.bc -ldl -lm -pthread -o $DST/sqlite.transformed >/dev/null
+${ALASKA}/tools/alaska $DST/sqlite.base.bc -ldl -lm -pthread -o $DST/sqlite.transformed >/dev/null
