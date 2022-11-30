d59b19eceebc34a305e6e51d15cb0726916b84e9
Nick Wanninger <nickwanninger@gmail.com>
2022-11-30 16:13:56 -0600

    re-add jemalloc, and use it for alaska allocation

--- runtime/include/alaska/internal.h
+++ runtime/include/alaska/internal.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
+#include <alaska/list_head.h>
 
 // This file contains structures that the the translation subsystem uses
 #ifdef __cplusplus
@@ -11,20 +11,41 @@ extern "C" {
 #endif
 
 #ifdef ALASKA_SANITY_CHECK
-#define ALASKA_SANITY(c, msg)
+#define ALASKA_SANITY(c, msg)                                                                   \
+  do {                                                                                          \
+    if (!(c)) { /* if the check is not true... */                                               \
+      fprintf(stderr, "\x1b[31m-----------[ Alaska Sanity Check Failed ]-----------\x1b[0m\n"); \
+      fprintf(stderr, "%s line %d\n", __FILE__, __LINE__);                                      \
+      fprintf(stderr, "%s (`%s` is false)\n", msg, #c);                                         \
+      fprintf(stderr, "\x1b[31m----------------------------------------------------\x1b[0m\n"); \
+      exit(EXIT_FAILURE);                                                                       \
+    }                                                                                           \
+  } while (0);
 #else
-#define ALASKA_SANITY(c, msg)  // do nothing if it's disabled
+#define ALASKA_SANITY(c, msg) /* do nothing if it's disabled */
 #endif
 
 
 typedef struct {
   void *ptr;  // The raw backing memory of the handle
   uint32_t usage_timestamp;
+  // struct list_head lru_ent;
   uint32_t size;
-  uint16_t locks;  // How many users does this handle have?
-  uint16_t flags;  // additional flags (idk, tbd)
+  uint64_t locks;  // How many users does this handle have?
+  // uint16_t flags;  // additional flags (idk, tbd)
 } alaska_mapping_t;
 
+extern alaska_mapping_t *alaska_map;
+extern size_t alaska_map_size;
+
+#define MAP_ENTRY_SIZE sizeof(alaska_mapping_t)  // enforced by a static assert in translation_types.h
+#define MAP_GRANULARITY 0x200000LU               // minimum size of a "huge page" if we ever get around to that
+#define HANDLE_MARKER (1LLU << 63)
+#define GET_ENTRY(handle) ((alaska_mapping_t *)((((uint64_t)(handle)) & ~HANDLE_MARKER) >> 32))
+#define GET_OFFSET(handle) ((off_t)(handle)&0xFFFFFFFF)
+
+#define ENT_GET_CANONICAL(ent) (((off_t)(ent) - (off_t)(alaska_map)) / MAP_ENTRY_SIZE)
+#define GET_CANONICAL(handle) ENT_GET_CANONICAL(GET_ENTRY(handle))
 
 #ifdef __cplusplus
 }
--- runtime/src/runtime.c
+++ runtime/src/runtime.c
@@ -10,26 +10,20 @@
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
+#include <jemalloc/jemalloc.h>
 
 #define likely(x) __builtin_expect((x), 1)
 #define unlikely(x) __builtin_expect((x), 0)
 
-#define MAP_ENTRY_SIZE sizeof(alaska_mapping_t)  // enforced by a static assert in translation_types.h
-#define MAP_GRANULARITY 0x200000LU               // minimum size of a "huge page" if we ever get around to that
-#define HANDLE_MARKER (1LLU << 63)
-#define GET_ENTRY(handle) ((alaska_mapping_t *)((((uint64_t)(handle)) & ~HANDLE_MARKER) >> 32))
-#define GET_OFFSET(handle) ((off_t)(handle)&0xFFFFFFFF)
 
-#define ENT_GET_CANONICAL(ent) (((off_t)(ent) - (off_t)(map)) / MAP_ENTRY_SIZE)
-#define GET_CANONICAL(handle) ENT_GET_CANONICAL(GET_ENTRY(handle))
 
 
 // convert a canonical handle id to a pointer.
-#define FROM_CANONICAL(chid) (&map[chid])
+#define FROM_CANONICAL(chid) (&alaska_map[chid])
 // How many map cells are there in the map table?
-size_t map_size = 0;
+size_t alaska_map_size = 0;
 // The memory for the alaska translation map. This lives at 0x200000 and is grown in 2mb chunks
-static alaska_mapping_t *map = NULL;
+alaska_mapping_t *alaska_map = NULL;
 
 // ...
 static uint64_t next_usage_timestamp = 0;
@@ -45,10 +39,9 @@ static uint64_t next_usage_timestamp = 0;
 static alaska_mapping_t *next_handle = NULL;
 
 
-extern "C" void *halloc(size_t sz) {
+void *halloc(size_t sz) {
   assert(sz < (1LLU << 32));
   uint64_t handle = HANDLE_MARKER | (((uint64_t)next_handle) << 32);
-
 
   alaska_mapping_t *ent = next_handle;
   next_handle = (alaska_mapping_t *)ent->ptr;
@@ -59,34 +52,80 @@ extern "C" void *halloc(size_t sz) {
 
   memset(ent, 0, MAP_ENTRY_SIZE);
   ent->size = sz;
-  // ent->ptr = NULL;  // don't allocate now. Do it later.
-  ent->ptr = malloc(sz);  // just use malloc
+  ent->ptr = je_malloc(sz);  // just use malloc
 
   return (void *)handle;
 }
 
 // Reallocate a handle
-extern "C" void *hrealloc(void *handle, size_t sz) {
-  auto ent = GET_ENTRY(handle);
-  ent->ptr = realloc(ent->ptr, sz);
+void *hrealloc(void *handle, size_t sz) {
+  alaska_mapping_t *ent = GET_ENTRY(handle);
+  ent->ptr = je_realloc(ent->ptr, sz);
   ent->size = sz;
   // realloc in alaska will always return the same pointer...
   // Ahh the benefits of using handles!
-  //
+
   // hopefully, nobody is relying on the output of realloc changing :I
   return handle;
 }
 
-extern "C" void hfree(void *ptr) {
-  auto ent = GET_ENTRY(ptr);
+void hfree(void *ptr) {
+  alaska_mapping_t *ent = GET_ENTRY(ptr);
   // assert(ent->locks == 0);
-  free(ent->ptr);
+  je_free(ent->ptr);
   memset(ent, 0, MAP_ENTRY_SIZE);
   ent->size = 0;
   ent->ptr = (void *)next_handle;
+
   next_handle = (alaska_mapping_t *)ent;
 }
 
+
+/* Use 'MADV_DONTNEED' to release memory to operating system quickly.
+ * We do that in a fork child process to avoid CoW when the parent modifies
+ * these shared pages. */
+void alaska_madvise_dontneed(alaska_mapping_t *ent) {
+#if defined(__linux__)
+  const size_t page_size = 4096;
+  size_t page_size_mask = page_size - 1;
+  void *ptr = ent->ptr;
+  size_t real_size = ent->size;
+  if (real_size < page_size) return;
+
+  /* we need to align the pointer upwards according to page size, because
+   * the memory address is increased upwards and we only can free memory
+   * based on page. */
+  char *aligned_ptr = (char *)(((size_t)ptr + page_size_mask) & ~page_size_mask);
+  real_size -= (aligned_ptr - (char *)ptr);
+  if (real_size >= page_size) {
+    madvise((void *)aligned_ptr, real_size & ~page_size_mask, MADV_DONTNEED);
+  }
+#endif
+}
+
+
+void *alaska_defrag_alloc(alaska_mapping_t *ent) {
+  void *ptr = ent->ptr;
+  size_t size = ent->size;
+  void *newptr;
+  /*
+	// TODO:
+  if(!je_get_defrag_hint(ptr)) {
+      server.stat_active_defrag_misses++;
+      return NULL;
+  }
+  */
+
+  /* move this allocation to a new allocation.
+   * make sure not to use the thread cache. so that we don't get back the same
+   * pointers we try to free */
+  // size = je_malloc_size(ptr);
+  newptr = je_mallocx(size, MALLOCX_TCACHE_NONE);
+  memcpy(newptr, ptr, size);
+  je_free(ptr);
+	ent->ptr = newptr;
+  return newptr;
+}
 
 enum alaska_fault_reason {
   NOT_PRESENT,
@@ -99,7 +138,7 @@ enum alaska_fault_reason {
 // may go out to disk, decompress, decrypt, or something else more interesting :)
 //
 // This, and alaska_barrier are the main locations where the runtime can be customized
-extern "C" __declspec(noinline) void alaska_fault(alaska_mapping_t *ent, alaska_fault_reason reason, off_t offset) {
+__declspec(noinline) void alaska_fault(alaska_mapping_t *ent, enum alaska_fault_reason reason, off_t offset) {
   if (reason == OUT_OF_BOUNDS) {
     fprintf(stderr, "[FATAL] alaska: out of bound access of handle %ld. Attempt to access byte %zu in a %d byte handle!\n",
         ENT_GET_CANONICAL(ent), offset, ent->size);
@@ -117,84 +156,31 @@ extern "C" __declspec(noinline) void alaska_fault(alaska_mapping_t *ent, alaska_
   exit(-1);
 }
 
-
-template <typename Fn>
-static void foreach_handle(Fn fn) {
-  for (size_t i = 0; i < map_size; i++) {
-    auto *ent = &map[i];
-    if (ent->size != 0) {
-      if (!fn(ent)) return;
-    }
-  }
-}
-
-
 #define MAX_MOVE 32
 
 int alaska_ent_usage_compare(const void *_a, const void *_b) {
-  auto a = *(alaska_mapping_t **)_a;
-  auto b = *(alaska_mapping_t **)_b;
+  alaska_mapping_t *a = *(alaska_mapping_t **)_a;
+  alaska_mapping_t *b = *(alaska_mapping_t **)_b;
 
   return a->usage_timestamp - b->usage_timestamp;
 }
 
 
 int alaska_ent_ptr_compare(const void *_a, const void *_b) {
-  auto a = *(void **)_a;
-  auto b = *(void **)_b;
+  void *a = *(void **)_a;
+  void *b = *(void **)_b;
   return (off_t)a - (off_t)b;
 }
 
 // This just means that the application has decided it is willing to let alaska
 // potentially stop the world and do what it needs to do. This function doesn't
 // do anything yet, but it could :)
-extern "C" __declspec(noinline) void alaska_barrier(void) {
-  // What size allocation are we looking to relocate?
-  uint32_t size_target = 16;
-  // How many have we found?
-  int found = 0;
-  // these are the ones we are interested in
-  alaska_mapping_t *relocation_plan[MAX_MOVE];
-  void *allocations[MAX_MOVE];
-
-  printf("=== alaska_barrier ===\n");
-  // Work through `map` and do something fun
-  foreach_handle([&](alaska_mapping_t *ent) {
-    if (found >= MAX_MOVE) return false;
-
-    if (ent->locks == 0 && ent->size == size_target) {
-      allocations[found] = ent->ptr;
-      relocation_plan[found++] = ent;
-    }
-    return true;
-  });
-
-  if (found == 0) return;
-
-
-  printf("movement plan:\n");
-  for (int i = 0; i < found; i++) {
-    auto ent = relocation_plan[i];
-    printf(" %6lu = { usage:%-4d ptr:%p }\n", ENT_GET_CANONICAL(ent), ent->usage_timestamp, ent->ptr);
-  }
-
-
-  qsort(relocation_plan, found, sizeof(alaska_mapping_t *), alaska_ent_usage_compare);
-
-  printf("relocated:\n");
-  for (int i = 0; i < found; i++) {
-    auto ent = relocation_plan[i];
-    printf(" %6lu = { usage:%-4d ptr:%p }\n", ENT_GET_CANONICAL(ent), ent->usage_timestamp, ent->ptr);
-  }
-
-
-  printf("----------------------\n");
-}
+__declspec(noinline) void alaska_barrier(void) {}
 
 
 // The core function to lock a handle. This is called only once we know
 // that @ptr is a handle (indicated by the top bit being set to 1).
-extern "C" void *alaska_guarded_lock(void *ptr) {
+void *alaska_guarded_lock(void *ptr) {
   // printf("lock %p, %ld\n", ptr, GET_CANONICAL(ptr));
   // A handle is encoded as two 32 bit components. The first (top half)
   // component is simply a pointer to the mapping directly. The second
@@ -205,9 +191,14 @@ extern "C" void *alaska_guarded_lock(void *ptr) {
   // On AAarch64, this operation can be performed with 3 instructions:
   //   ubfx; ldr; add;
   // (Extract bits (32..63), load `ent->ptr`, and add the offset)
-  auto ent = GET_ENTRY(ptr);
+  alaska_mapping_t *ent = GET_ENTRY(ptr);
+  ALASKA_SANITY(ent >= alaska_map, "Entry is before the map");
+  ALASKA_SANITY(ent < alaska_map + alaska_map_size, "Entry is after the map");
+
   // Extract the lower 32 bits of the pointer for the offset.
   off_t off = GET_OFFSET(ptr);
+
+
 
 #ifdef ALASKA_OOB_CHECK
   // Alaska can perform out of bounds checks when each load and store
@@ -231,7 +222,9 @@ extern "C" void *alaska_guarded_lock(void *ptr) {
   //   alaska_fault(ent, NOT_PRESENT, off);
   // }
 
-  // Proxy for temporal locality
+  // Proxy for temporal locality.
+  // The compiler could decide to do this or not based on if it
+  // if wants to track this in some scope
   ent->usage_timestamp = next_usage_timestamp++;
 
   // Record the lock occuring so the runtime knows not to relocate the memory.
@@ -246,12 +239,16 @@ extern "C" void *alaska_guarded_lock(void *ptr) {
 }
 
 
-extern "C" void alaska_guarded_unlock(void *ptr) {
-  auto ent = GET_ENTRY(ptr);
+void alaska_guarded_unlock(void *ptr) {
+  alaska_mapping_t *ent = GET_ENTRY(ptr);
   ent->locks--;
 }
 
-extern "C" void *alaska_lock(void *ptr) {
+
+
+// These functions are simple wrappers around the guarded version of the
+// same name. These versions just check if `ptr` is a handle before locking.
+void *alaska_lock(void *ptr) {
   uint64_t h = (uint64_t)ptr;
   if (likely((h & HANDLE_MARKER) != 0)) {
     return alaska_guarded_lock(ptr);
@@ -276,30 +273,20 @@ static void __attribute__((constructor)) alaska_init(void) {
   size_t sz = MAP_GRANULARITY * 8;
 
   // TODO: do this using hugetlbfs :)
-  map = (alaska_mapping_t *)mmap((void *)MAP_GRANULARITY, sz, PROT_READ | PROT_WRITE, flags | MAP_FIXED, fd, 0);
-  map_size = sz / MAP_ENTRY_SIZE;
+  alaska_map = (alaska_mapping_t *)mmap((void *)MAP_GRANULARITY, sz, PROT_READ | PROT_WRITE, flags | MAP_FIXED, fd, 0);
+  alaska_map_size = sz / MAP_ENTRY_SIZE;
 
-  for (size_t i = 0; i < map_size - 1; i++) {
+  for (size_t i = 0; i < alaska_map_size - 1; i++) {
     // when unmapped, the pointer field points to the next free handle
-    map[i].ptr = (void *)&map[i + 1];
-    map[i].size = 0;  // a size of zero indicates that this field is unmapped
+    alaska_map[i].ptr = (void *)&alaska_map[i + 1];
+    alaska_map[i].size = 0;  // a size of zero indicates that this field is unmapped
   }
-  map[map_size - 1].ptr = NULL;
+  alaska_map[alaska_map_size - 1].ptr = NULL;
   // start allocating at the first handle
-  next_handle = &map[0];
+  next_handle = &alaska_map[0];
 }
 
-static void __attribute__((destructor)) alaska_deinit(void) { munmap(map, map_size * MAP_ENTRY_SIZE); }
-
-extern "C" void *hmalloc_map_frame(int level, size_t entry_size, int size) {
-  void *p = calloc(entry_size, size);
-  return p;
+static void __attribute__((destructor)) alaska_deinit(void) {
+  // Simply unmap the map region
+  munmap(alaska_map, alaska_map_size * MAP_ENTRY_SIZE);
 }
-
-
-void *operator new(size_t size) {
-  void *ptr = malloc(size);
-  return ptr;
-}
-
-void operator delete(void *ptr) { free(ptr); }
