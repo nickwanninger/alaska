d5770021bf9fcafc9dcbd975df8faf9d317ccd84
Nick Wanninger <nickwanninger@gmail.com>
2023-01-02 20:30:51 -0600

    alaska-config

---
+++ compiler/bin/alaska-config.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <set>
+#include <string>
+#include <map>
+#include <vector>
+
+#if defined(__aarch64__)
+static const char *arch = "aarch64";
+#elif defined(__x86_64__)
+static const char *arch = "x86_64";
+#endif
+
+
+static const char *local = ALASKA_INSTALL_PREFIX;
+
+typedef void (*flag_handler_t)(void);
+
+
+void prefix(void) { printf("%s\n", local); }
+void linkdir(void) { printf("%s/lib\n", local); }
+void includedir(void) { printf("%s/include\n", local); }
+
+
+void ldflags(void) {
+	printf("-T %s/share/alaska/ldscripts/alaska-%s.ld\n", local, arch);
+  printf("-L%s/lib\n", local);
+  printf("%s/lib/libalaska_core.a\n", local);
+  printf("-Wl,-rpath=%s/lib\n", local);
+  printf("-Wl,-rpath-link=%s/lib\n", local);
+  printf("-lm\n");
+  printf("-lalaska_support\n");
+  printf("-lunicorn\n");
+}
+
+
+void includeflags(void) { printf("-I%s/include\n", local); }
+
+void cflags(void) {
+	includeflags();
+}
+
+void cxxflags(void) {
+  cflags();
+  printf("-fno-exceptions\n");
+}
+
+void defs(void) {
+	// enable a bunch of feature flags
+  printf("-D_ALASKA\n");
+  printf("-D_GNU_SOURCE\n");
+  printf("-D__STDC_CONSTANT_MACROS\n");
+  printf("-D__STDC_FORMAT_MACROS\n");
+  printf("-D__STDC_LIMIT_MACROS\n");
+}
+
+
+// clang-format off
+std::map<std::string, std::vector<flag_handler_t>> arg_required = {
+    {"--prefix", {prefix}},
+    {"--linkdir", {linkdir}},
+    {"--includedir", {includedir}},
+    {"--ldflags", {ldflags}},
+    {"--cflags", {cflags, defs}},
+    {"--cxxflags", {cxxflags, defs}},
+};
+// clang-format on
+
+void usage(void) {
+  printf("usage: alaska-config <OPTION>...\n");
+  for (auto &[key, val] : arg_required) {
+    printf("  %s: ...\n", key.data());
+  }
+}
+
+int main(int argc, char **argv) {
+  std::set<flag_handler_t> required;
+
+
+  if (argc == 1) {
+    usage();
+    return EXIT_FAILURE;
+  }
+
+  for (int i = 1; i < argc; i++) {
+    std::string arg = argv[i];
+    auto f = arg_required.find(arg);
+    if (f == arg_required.end()) {
+      usage();
+      return EXIT_FAILURE;
+    }
+
+    for (auto flag : f->second) {
+      required.insert(flag);
+    }
+  }
+
+  for (auto req : required) {
+    req();
+  }
+  return EXIT_SUCCESS;
+}
