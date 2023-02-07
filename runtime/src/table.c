759f4416b1549e857e1897b4d4c752ed7fcc4e65
Nick Wanninger <nickwanninger@gmail.com>
2023-02-06 20:33:26 -0600

    runtime: re-add locks to table allocation

--- runtime/src/table.c
+++ runtime/src/table.c
@@ -11,6 +11,9 @@ static size_t table_nfree;  // how many entries are free?
 static alaska_mapping_t *table_memory;
 static alaska_mapping_t *next_free;
 
+
+static void alaska_table_put_r(alaska_mapping_t *ent);
+
 #define MAP_ENTRY_SIZE sizeof(alaska_mapping_t)  // enforced by a static assert in translation_types.h
 // #define MAP_GRANULARITY 0x200000LU               // minimum size of a "huge
 // page" if we ever get around to that
@@ -63,7 +66,7 @@ static void alaska_table_grow() {
   size_t newsize = newbytes / MAP_ENTRY_SIZE;
 
   for (size_t i = newsize - 1; i > table_size; i--) {
-    alaska_table_put(&table_memory[i]);
+    alaska_table_put_r(&table_memory[i]);
   }
 
   // update the total size
@@ -73,8 +76,11 @@ static void alaska_table_grow() {
 }
 
 void alaska_table_init(void) {
+  pthread_mutex_init(&table_lock, NULL);
+  pthread_mutex_lock(&table_lock);
   next_free = NULL;
   alaska_table_grow();
+  pthread_mutex_unlock(&table_lock);
 }
 
 void alaska_table_deinit(void) { munmap(table_memory, table_size * MAP_ENTRY_SIZE); }
@@ -82,9 +88,16 @@ void alaska_table_deinit(void) { munmap(table_memory, table_size * MAP_ENTRY_SIZ
 unsigned alaska_table_get_canonical(alaska_mapping_t *ent) { return (unsigned)(ent - table_memory); }
 alaska_mapping_t *alaska_table_from_canonical(unsigned canon) { return &table_memory[canon]; }
 
+static void alaska_table_put_r(alaska_mapping_t *ent) {
+  ent->size = -1;
+  ent->ptr = next_free;
+  table_nfree++;
+  next_free = ent;
+}
+
 // allocate a table entry
 alaska_mapping_t *alaska_table_get(void) {
-  // pthread_mutex_lock(&table_lock);
+  pthread_mutex_lock(&table_lock);
   alaska_mapping_t *ent = NULL;
 
   if (next_free == NULL) {
@@ -97,30 +110,13 @@ alaska_mapping_t *alaska_table_get(void) {
 
   memset(ent, 0, sizeof(*ent));
 
-  // pthread_mutex_unlock(&table_lock);
+  pthread_mutex_unlock(&table_lock);
   return ent;
-
-  // // slow and bad, but correct
-  // for (size_t i = 0; i < size; i++) {
-  //   alaska_mapping_t *ent = &map[i];
-  //   if (ent->size == -1) {
-  //     ent->size = 0;
-  //     nfree--;
-  //     // printf("nfree=%zu\n", nfree);
-  //     return ent;
-  //   }
-  // }
-  // alaska_table_grow();
-  // return alaska_table_get();
-  // return NULL;
 }
 
 // free a table entry
 void alaska_table_put(alaska_mapping_t *ent) {
-  // pthread_mutex_lock(&table_lock);
-  ent->size = -1;
-  ent->ptr = next_free;
-  table_nfree++;
-  next_free = ent;
-  // pthread_mutex_unlock(&table_lock);
+  pthread_mutex_lock(&table_lock);
+	alaska_table_put_r(ent);
+  pthread_mutex_unlock(&table_lock);
 }
