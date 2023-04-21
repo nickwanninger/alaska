523c961173821f18a56f00fac90dac0c4c10d812
Nick Wanninger <nickwanninger@gmail.com>
2023-04-20 20:33:51 -0500

    runtime: run __builtin_prefetch

--- runtime/lib/prefetch.c
+++ runtime/lib/prefetch.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include <alaska/internal.h>
 
-// The definition of what a handle's bits mean when they are used like a pointer
-typedef union {
-  struct {
-    unsigned long offset : 63;
-    unsigned flag : 1;
-  };
-  void *ptr;
-} flagged_pointer_t;
 
 ALASKA_INLINE void *alaska_prefetch_translate(void *ptr) {
-  flagged_pointer_t h;
-  h.ptr = ptr;
-  if (h.flag) {
-    printf("Prefetch %p\n", ptr);
-  }
+  // TODO:
+  __builtin_prefetch(ptr);
   return ptr;
 }
 
