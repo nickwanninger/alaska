ae39d3cc883f2b69f5b62e472005dbc12df17540
Nick Wanninger <nickwanninger@gmail.com>
2023-10-24 11:51:43 -0500

    menuconfig: write to a new location

--- tools/menuconfig.py
+++ tools/menuconfig.py
@@ -1849,8 +1849,8 @@ class Kconfig(object):
             f.write(contents)
 
         # write the autoconf file
-        self.write_autoconf('runtime/include/alaska/config.h')
-        self.write_autoconf('compiler/include/config.h')
+        self.write_autoconf('runtime/include/alaska/autoconf.h')
+        # self.write_autoconf('compiler/include/_config.h')
         if self._write_if_changed('config.cmake', self._cmake_contents(header)):
             return "config.cmake saved".format(filename)
 
