9037d4ecb67bfcf0af4e81e9c7f9503f3d42be47
Nick Wanninger <nickwanninger@gmail.com>
2023-12-12 02:31:30 -0600

    fix unused includes in translate.cpp

--- runtime/core/translate.cpp
+++ runtime/core/translate.cpp
@@ -8,17 +8,12 @@
  * This is free software.  You are permitted to use, redistribute,
  * and modify it as specified in the file "LICENSE".
  */
-#include <assert.h>
-#include <errno.h>
 #include <execinfo.h>
 #include <fcntl.h>
 #include <malloc.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/mman.h>
-#include <time.h>
 #include <unistd.h>
 
 #include <alaska.h>
