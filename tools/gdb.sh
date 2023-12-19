6bbdfdeef1f8009678142f2bdb16a27535830b10
Nick Wanninger <nickwanninger@gmail.com>
2023-12-18 18:31:27 -0600

    tools/gdb.sh: enable running it from anywhere

--- tools/gdb.sh
+++ tools/gdb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-gdb-multiarch -ix tools/gdbinit $@
+ROOT=$(realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../")
+gdb-multiarch -ix ${ROOT}/tools/gdbinit $@
 
