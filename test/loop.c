fa9ade2e2e198162ed4f33217d1d406e83f6835e
Nick Wanninger <nickwanninger@gmail.com>
2024-01-09 19:06:58 -0600

    add a test program

--- test/loop.c
+++ test/loop.c
@@ -1,25 +1,14 @@
 #include <stdlib.h>
 
-// int sumarray(int* restrict vals, int len) {
-//   int sum = 0;
-//   for (int i = 0; i < len; i++) {
-// 		if (vals[i] > 10)
-//     	sum += vals[i];
-//   }
-//   return sum;
-// }
-
-void squarearray(int* restrict vals, int len) {
+int sumarray(int* restrict vals, int len) {
+  int sum = 0;
   for (int i = 0; i < len; i++) {
-    vals[i] *= vals[i];
+    sum += vals[i];
   }
+  return sum;
 }
 
+
 int main() {
-  int array[32];
-  for (int i = 0; i < 32; i++) {
-    i = rand();
-  }
-  squarearray(array, 32);
   return 0;
 }
