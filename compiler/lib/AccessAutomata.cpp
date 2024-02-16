3f4eb02a73fdf43883a6f0c96b604da8d2be3994
Nick Wanninger <nickwanninger@gmail.com>
2024-02-16 09:41:24 -0600

    AccessAutomata: construct a memory offset map
    This is a mapping from an LLVM (memory) instruction to the set of byte
    offsets which it may/can access into a given SSA object. This will be
    useful when we later transition the AccessAutomata analysis into using
    offsets instead of just values
    
    Signed-off-by: Nick Wanninger <nickwanninger@gmail.com>

--- compiler/lib/AccessAutomata.cpp
+++ compiler/lib/AccessAutomata.cpp
@@ -19,6 +19,137 @@
 
 
 
+// Assuming you have a GetElementPtrInst* named gepInst
+static bool isFirstOffsetZero(GetElementPtrInst *gepInst) {
+  if (gepInst->getNumOperands() < 2) return false;
+  Value *firstIndexOperand = gepInst->getOperand(1);
+  if (ConstantInt *constantIndex = dyn_cast<ConstantInt>(firstIndexOperand)) {
+    return constantIndex->isZero();
+  }
+  return false;
+}
+
+
+static std::map<llvm::Instruction *, std::set<long>> memory_access_offsets(llvm::Value *obj) {
+  alaska::DirectedGraph<llvm::Value *, std::set<long>> g;
+
+  std::stack<llvm::Value *> q;
+  std::set<long> empty;
+  auto add = [&](llvm::Value *v) -> void {
+    if (not g.has_node(v)) {
+      q.push(v);
+      g.add_nodes(v);
+    }
+  };
+
+  add(obj);
+
+  // Construct the use-def graph without labeling their edges.
+  while (not q.empty()) {
+    auto v = q.top();
+    q.pop();
+
+    for (auto user : v->users()) {
+      if (auto gep = dyn_cast<GetElementPtrInst>(user)) {
+        if (isFirstOffsetZero(gep)) {
+          add(user);
+          g.add_edge_with_prop(v, user, empty);
+        }
+      } else if (isa<PHINode>(user)) {
+        add(user);
+        g.add_edge_with_prop(v, user, empty);
+      } else if (isa<LoadInst>(user) or isa<StoreInst>(user)) {
+        g.add_nodes(dyn_cast<llvm::Value>(user));
+        g.add_edge_with_prop(v, user, empty);
+      }
+    }
+  }
+
+
+  // Condense the graph to eliminate cycles and produce a DAG
+  // TODO: Validate that this makes sense.
+  auto con = g.condense();
+
+  // Compute the topological order in which to flow offsets
+  auto order_o = con.topological_order();
+  ALASKA_SANITY(order_o.has_value(), "A condensed graph must have a topo order");
+  auto order = order_o.value();
+
+
+  // Iterate over that order, flowing incoming offests to the outgoing edges w/ some
+  // node-specific transformation.
+  for (auto *node : order) {
+    std::set<long> all_incoming;
+    // if there are no incoming edges, insert an offset of 0
+    if (con.count_in_neighbors(node) == 0) {
+      all_incoming.insert(0);
+    } else {
+      for (auto &[_, e] : con.incoming(node)) {
+        all_incoming.insert(e.prop().begin(), e.prop().end());
+      }
+    }
+    // Apply node-specific transformations
+    if (auto gep = dyn_cast<llvm::GetElementPtrInst>(node)) {
+      llvm::APInt offset(64, 0, true);
+
+      auto DL = gep->getModule()->getDataLayout();
+
+      if (gep->accumulateConstantOffset(DL, offset)) {
+        int signedOffset = offset.getSExtValue();
+        std::set<long> transformed;
+        for (auto &e : all_incoming) {
+          transformed.insert(e + signedOffset);
+        }
+
+        all_incoming = std::move(transformed);
+      } else {
+        // If we could not accumulate a constant offset, wipe out the all_incoming.
+        all_incoming.clear();
+      }
+    }
+
+    for (auto &[_, e] : con.outgoing(node)) {
+      // Flow that incoming to the out edges.
+      e.prop().insert(all_incoming.begin(), all_incoming.end());
+    }
+  }
+
+
+  // alaska::println("Flow graph:");
+  // alaska::println("digraph {");
+  // alaska::println("  compound = true;");
+  //
+  // for (auto *v : order) {
+  //   alaska::println("  n", v, "[label=\"", *v, "\"]");
+  // }
+  //
+  // for (auto &from : order) {
+  //   for (auto &[to, edge] : con.outgoing(from)) {
+  //     alaska::print("   n", from, " -> n", to, " [label=\"");
+  //     for (auto e : edge.prop()) {
+  //       alaska::print(" ", e);
+  //     }
+  //     alaska::println("\", shape=box];");
+  //   }
+  // }
+  // alaska::println("}");
+
+  std::map<llvm::Instruction *, std::set<long>> res;
+
+  for (auto *inst : order) {
+    if (isa<LoadInst>(inst) or isa<StoreInst>(inst)) {
+      std::set<long> offsets;
+      for (auto &[_, e] : con.incoming(inst)) {
+        offsets.insert(e.prop().begin(), e.prop().end());
+      }
+      res[dyn_cast<Instruction>(inst)] = std::move(offsets);
+    }
+  }
+  return res;
+}
+
+
+
 alaska::AccessAutomata::AccessAutomata(llvm::Value *object, alaska::OptimisticTypes &OT) {
   using namespace alaska::re;
   llvm::Function *func = nullptr;
