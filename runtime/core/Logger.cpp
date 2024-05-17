f287d0d47f7fb27e919b83a55c620dc430371da6
Nick Wanninger <nickwanninger@gmail.com>
2024-05-17 10:48:02 -0500

    Add a small logging framework

---
+++ runtime/core/Logger.cpp
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the Alaska Handle-Based Memory Management System
+ *
+ * Copyright (c) 2024, Nick Wanninger <ncw@u.northwestern.edu>
+ * Copyright (c) 2024, The Constellation Project
+ * All rights reserved.
+ *
+ * This is free software.  You are permitted to use, redistribute,
+ * and modify it as specified in the file "LICENSE".
+ */
+
+
+
+#include <alaska/Logger.hpp>
+#include <pthread.h>
+#include <stdarg.h>
+
+static const char *level_strings[] = {"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
+
+static const char *level_colors[] = {
+    "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"};
+
+
+static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+static int log_level = LOG_INFO;
+
+namespace alaska {
+  void log(int level, const char *file, int line, const char *fmt, ...) {
+    if (level >= log_level) {
+      va_list args;
+      va_start(args, fmt);
+
+      // Grab the lock
+      pthread_mutex_lock(&log_mutex);
+
+
+      time_t t = time(NULL);
+      auto time = localtime(&t);
+
+      char buf[16];
+      buf[strftime(buf, sizeof(buf), "%H:%M:%S", time)] = '\0';
+      fprintf(stderr, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ", buf, level_colors[level],
+          level_strings[level], file, line);
+      vfprintf(stderr, fmt, args);
+      fprintf(stderr, "\n");
+
+      // release the lock
+      pthread_mutex_unlock(&log_mutex);
+      va_end(args);
+    }
+  }
+
+
+  void set_log_level(int level) { log_level = level; }
+}  // namespace alaska
---
+++ runtime/include/alaska/Logger.hpp
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the Alaska Handle-Based Memory Management System
+ *
+ * Copyright (c) 2024, Nick Wanninger <ncw@u.northwestern.edu>
+ * Copyright (c) 2024, The Constellation Project
+ * All rights reserved.
+ *
+ * This is free software.  You are permitted to use, redistribute,
+ * and modify it as specified in the file "LICENSE".
+ */
+
+
+#pragma once
+
+#include <stdio.h>
+
+enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
+
+
+namespace alaska {
+  void log(int level, const char *file, int line, const char *fmt, ...);
+
+
+  void set_log_level(int level);
+};  // namespace alaska
+
+// clang-format off
+#define log_trace(...) alaska::log(LOG_TRACE, __FILE_NAME__, __LINE__, __VA_ARGS__)
+#define log_debug(...) alaska::log(LOG_DEBUG, __FILE_NAME__, __LINE__, __VA_ARGS__)
+#define log_info(...)  alaska::log(LOG_INFO,  __FILE_NAME__, __LINE__, __VA_ARGS__)
+#define log_warn(...)  alaska::log(LOG_WARN,  __FILE_NAME__, __LINE__, __VA_ARGS__)
+#define log_error(...) alaska::log(LOG_ERROR, __FILE_NAME__, __LINE__, __VA_ARGS__)
+#define log_fatal(...) alaska::log(LOG_FATAL, __FILE_NAME__, __LINE__, __VA_ARGS__)
+// clang-format on
