96411aef4cf495abc5b05f736621825e6ce00c0c
Atmn Patel <atmndp@gmail.com>
2024-08-29 15:21:17 -0500

    [Yukon] Adding build script

---
+++ tools/build_yukon.sh
@@ -0,0 +1,22 @@
+
+
+unset LD_LIBRARY_PATH
+
+ROOT=$RISCV
+
+export CC=$ROOT/bin/riscv64-unknown-linux-gnu-gcc
+export CXX=$ROOT/bin/riscv64-unknown-linux-gnu-g++
+export AS=$ROOT/bin/riscv64-unknown-linux-gnu-as
+export LD=$ROOT/bin/riscv64-unknown-linux-gnu-ld
+
+make clean
+
+cmake ../ \
+      -DALASKA_ENABLE_COMPILER=OFF \
+      -DALASKA_ENABLE_TESTING=OFF \
+      -DALASKA_CORE_ONLY=ON \
+      -DALASKA_YUKON=ON \
+      -DALASKA_SIZE_BITS=32 \
+      -DCMAKE_SYSROOT=$ROOT/sysroot
+
+make -j
