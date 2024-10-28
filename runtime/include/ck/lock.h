b4c6ac260dfd8cd188c49594a6a42d340e1dcfeb
Nick Wanninger <nickwanninger@gmail.com>
2024-10-28 18:52:27 -0500

    simplify scoped_lock

--- runtime/include/ck/lock.h
+++ runtime/include/ck/lock.h
@@ -9,26 +9,15 @@ namespace ck {
     pthread_mutex_t m_mutex;
 
    public:
-    mutex(void) {
-      pthread_mutex_init(&m_mutex, NULL);
-    }
+    mutex(void) { pthread_mutex_init(&m_mutex, NULL); }
 
-    void init(void) {
-      pthread_mutex_init(&m_mutex, NULL);
-    }
-    ~mutex(void) {
-      pthread_mutex_destroy(&m_mutex);
-    }
-    int try_lock(void) {
-      return pthread_mutex_trylock(&m_mutex);
-    }
+    void init(void) { pthread_mutex_init(&m_mutex, NULL); }
+    ~mutex(void) { pthread_mutex_destroy(&m_mutex); }
+    int try_lock(void) { return pthread_mutex_trylock(&m_mutex); }
 
-    void lock(void) {
-      pthread_mutex_lock(&m_mutex);
-    }
-    void unlock(void) {
-      pthread_mutex_unlock(&m_mutex);
-    }
+
+    void lock(void) { pthread_mutex_lock(&m_mutex); }
+    void unlock(void) { pthread_mutex_unlock(&m_mutex); }
   };
 
 
@@ -39,13 +28,11 @@ namespace ck {
    public:
     inline scoped_lock(ck::mutex &lck)
         : lck(lck) {
-      locked = true;
       lck.lock();
     }
 
-    inline ~scoped_lock(void) {
-      unlock();
-    }
+    inline ~scoped_lock(void) { lck.unlock(); }
+  };
 
     inline void unlock(void) {
       if (locked) lck.unlock();
